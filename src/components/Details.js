5855b1317df7ecc8db5e16914af73566b12f1762
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-04 12:33:20 -0600

    wip

--- src/components/Details.js
+++ src/components/Details.js
@@ -30,7 +30,10 @@ export default function Details(props) {
   }, []);
 
   // TASK 6 - Create a side effect 🥵 that runs after every render.
-  useEffect(() => )
+  useEffect(() => {
+    console.log("effect after every DOM surgery");
+    // return
+  });
 
   // TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
