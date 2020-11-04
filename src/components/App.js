4a90d66aa9654f9ae59b749a14270e9118e69be5
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-11-04 12:16:01 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 // TASK 1 - import the axios lib from node_modules
-
+import axios from "axios";
 // TASK 2 - import the contants from constants/index.js
 import { BASE_URL, API_KEY } from "../constants";
 import Details from "./Details";
